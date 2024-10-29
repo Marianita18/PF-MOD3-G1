72df590a6eec9d23f57706367efd9deec3ff0768
EugeSan-hub <eugesans@gmail.com>
2024-10-28 22:27:10 -0300

    Revert "Validacion formulario footer"

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,43 @@
-import Index from "./components/pages/Index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/common/Navbar";
+import NavBar from "./components/common/Navbar";
+
+import { Index } from "./page/Index";
+import QuienesSomos from "./components/pages/QuienesSomos";
 import GaleriaImagen from "./components/pages/GaleriaImagen";
-import SobreNosotros from "./components/pages/SobreNosotros";
 import CatalogoHabitacion from "./components/pages/CatalogoHabitacion";
 import SuiteStandard from "./components/pages/SuiteStandard";
 import SuiteJunior from "./components/pages/SuiteJunior";
-import SuitePremiun from "./components/pages/SuitePremiun";
 import Contacto from "./components/pages/Contacto";
-import Error404 from "./components/pages/Error404";
 import Footer from "./components/common/Footer";
+import Error404 from "./components/pages/Error404";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
-import Servicios from "./components/pages/Servicios";
-
 function App() {
   return (
     <BrowserRouter>
-      <Navbar></Navbar>
+   <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<Index></Index>}></Route>
         <Route
-          path="/sobreNosotros"
-          element={<SobreNosotros></SobreNosotros>}
-        ></Route>
+          path="/QuienesSomos"
+          element={<QuienesSomos></QuienesSomos>}></Route>
         <Route
-          path="/galeriaImagenes"
-          element={<GaleriaImagen></GaleriaImagen>}
-        ></Route>
+          path="/GaleriaImagen"
+          element={<GaleriaImagen></GaleriaImagen>}></Route>
         <Route
-          path="/catalogoHabitaciones"
-          element={<CatalogoHabitacion></CatalogoHabitacion>}
-        ></Route>
+          path="/CatalogoHabitacion"
+          element={<CatalogoHabitacion></CatalogoHabitacion>}></Route>
         <Route
-          path="/suiteStandard"
-          element={<SuiteStandard></SuiteStandard>}
-        ></Route>
+          path="/SuiteStandard"
+          element={<SuiteStandard></SuiteStandard>}></Route>
         <Route
-          path="/suiteJunior"
-          element={<SuiteJunior></SuiteJunior>}
-        ></Route>
-        <Route path="/suitePremiun" element={<SuitePremiun></SuitePremiun>}></Route>
-        
-        <Route path="/contacto" element={<Contacto></Contacto>}></Route>
-        <Route path="/*" element={<Error404></Error404>}></Route>
+          path="/SuiteJunior"
+          element={<SuiteJunior></SuiteJunior>}></Route>
+        <Route path="/Contacto" element={<Contacto></Contacto>}></Route>
+        <Route path="/Error404" element={<Error404></Error404>}></Route>
       </Routes>
-      <Footer></Footer>
+    <Footer></Footer>
     </BrowserRouter>
   );
 }
--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,5 +1,6 @@
 
 export default function Index() {
-  return <div>Index</div>;
+  return (
+    <div>Index</div>
+  )
 }
-
