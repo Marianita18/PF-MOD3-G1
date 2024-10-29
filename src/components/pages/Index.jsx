573399874ec528bfb97e3e3847bec715d73eb47d
ismael <joseismaelvargas@users.noreply.github.com>
2024-10-29 15:06:24 -0300

    2

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,11 +1,13 @@
-import { Carrusel } from "./ComponenteIndex/Carrusel";
-import { Infoindex } from "./ComponenteIndex/Infoindex";
+// import { Carrusel } from "./ComponenteIndex/Carrusel";
+// import { Infoindex } from "./ComponenteIndex/Infoindex";
 
 export default  Index=()=> {
-  <main className="Informacion">
-    <h1>index</h1>
-  <Carrusel></Carrusel>
-  <Infoindex></Infoindex>
- </main>
+ <>
+     <h1>index</h1>
+  {/* <Carrusel></Carrusel>
+  <Infoindex></Infoindex> */}
+
+ </>
+
 }
 
