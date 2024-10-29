5d233babe9defceebd7683b3c43759c228822cd9
ismael <joseismaelvargas@users.noreply.github.com>
2024-10-29 15:45:49 -0300

    Se comitio

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -2,15 +2,19 @@ import { Carrusel } from "../pages/componentesIndex/Carrusel"
 import { Infoindex } from "../pages/componentesIndex/Infoindex"
 import { Restaurantee } from "../pages/componentesIndex/Restaurantee"
 import { Testimonios } from "../pages/componentesIndex/Testimonios"
+import "./componentesIndex/styleIndex/Infoindex.css"
 export default function Index() {
   return(
     <>
+    <main className="main">
     <Carrusel></Carrusel>
     <Infoindex></Infoindex>
     
     <Restaurantee></Restaurantee>
     <Testimonios></Testimonios>
-    <div>Index Este es el Index 20.1</div>;
+    </main>
+  
+    
   </>
   ) 
 
