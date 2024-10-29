ff24ed41ca8c23e30bd2eac80d161c59a9a11305
ismael <joseismaelvargas@users.noreply.github.com>
2024-10-29 15:15:45 -0300

    Se intalaron las imagenes

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,7 +1,8 @@
-
+import { Carrusel } from "./ComponenteIndex/Carrusel"
 export default function Index() {
   return(
     <>
+    <Carrusel></Carrusel>
     <div>Index Este es el Index 20.1</div>;
   </>
   ) 
